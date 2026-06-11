570c95a9eab60a215d4e8159dff9b871b7447ee3
sendinandi <sendiwildanto@gmail.com>
2026-06-11 22:10:02 +0700

    Setup portfolio for new GitHub repository

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
-/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  base: "/sendi-portfolio/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
