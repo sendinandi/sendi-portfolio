ddb21ce14a81a538f6cbff2758e8a3a291df06e3
sendinandi <sendiwildanto@gmail.com>
2026-06-11 22:40:46 +0700

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      <BrowserRouter basename="/sendi-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
